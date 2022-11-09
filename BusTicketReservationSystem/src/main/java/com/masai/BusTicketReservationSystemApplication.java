6b5d61411c967f1dded67771c245b41c61eac4bc
mayurisamanta <samantamayuri@gmail.com>
2022-11-09 22:09:36 +0530

    adding day 1

---
+++ BusTicketReservationSystem/src/main/java/com/masai/BusTicketReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTicketReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusTicketReservationSystemApplication.class, args);
+	}
+
+}
