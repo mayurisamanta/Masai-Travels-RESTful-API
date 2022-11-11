a91162b0029a44df10cb739663f6a63a93dced69
Sarthak0008 <98695968+Sarthak0008@users.noreply.github.com>
2022-11-11 23:14:12 +0530

    day3

--- BusTicketReservationSystem/src/main/java/com/masai/repository/BusDao.java
+++ BusTicketReservationSystem/src/main/java/com/masai/repository/BusDao.java
@@ -1,5 +1,7 @@
 package com.masai.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,6 @@ import com.masai.model.Bus;
 @Repository
 public interface BusDao extends JpaRepository<Bus, Integer> {
 	
-	
+	public List<Bus> findByBusType(String busType);
 
 }
