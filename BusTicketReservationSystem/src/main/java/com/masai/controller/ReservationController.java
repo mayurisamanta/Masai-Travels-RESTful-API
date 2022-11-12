6af2cdbc88a8578ea388c04531aa5da824cd66a0
anshul <anshulsharmaa.76@gmail.com>
2022-11-12 13:20:28 +0530

    project

--- BusTicketReservationSystem/src/main/java/com/masai/controller/ReservationController.java
+++ BusTicketReservationSystem/src/main/java/com/masai/controller/ReservationController.java
@@ -1,15 +1,21 @@
 package com.masai.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.masai.exception.ReservationException;
@@ -45,13 +51,50 @@ public class ReservationController {
 	}
 	
 	
-	@DeleteMapping("/reservation/{id}")
-	public ResponseEntity<Reservation> deleteReservationHandler(@PathVariable("Id") Integer reservationId) throws ReservationException{
+	@DeleteMapping("/reservations/{id}")
+	public ResponseEntity<Reservation> deleteReservationHandler(@PathVariable("id") Integer reservationId) throws ReservationException{
 		
 		Reservation deleteReservation = rService.deleteReservation(reservationId);
 		
 		return new ResponseEntity<Reservation>(deleteReservation, HttpStatus.OK);
 		
+		
+	}
+	
+	
+	
+	
+	@GetMapping("/reservations/{rid}")
+	public ResponseEntity<Reservation> viewReservationByIdHandlerHandler(@PathVariable("rid") Integer reservationId) throws ReservationException{
+		
+		Reservation viewReservation = rService.viewReservationById(reservationId);
+		
+		return new ResponseEntity<Reservation>(viewReservation, HttpStatus.OK);
+		
+		
+	}
+	
+	
+	@GetMapping("/reservations")
+	public ResponseEntity<List<Reservation>> viewAllReservationHandler() throws ReservationException{
+		
+		List<Reservation> reservations = rService.viewAllReservation();
+		
+		
+		return new ResponseEntity<List<Reservation>>(reservations, HttpStatus.OK);	
+		
+	}
+	
+	
+	@PostMapping("/reservationsbydate")
+	public ResponseEntity<List<Reservation>> getAllReservationByDate(@RequestParam("localdate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) throws ReservationException{
+		
+		
+		System.out.println(date);
+		
+		List<Reservation> reservation = rService.getAllReservationByDate(date);
+		
+		return new ResponseEntity<List<Reservation>>(reservation, HttpStatus.OK);
 		
 	}
 	
--- BusTicketReservationSystem/src/main/java/com/masai/service/IFeedbackService.java
+++ BusTicketReservationSystem/src/main/java/com/masai/service/IFeedbackService.java
@@ -8,7 +8,7 @@ import com.masai.model.Feedback;
 
 public interface IFeedbackService {
 
-	public Feedback addFeedback(Integer userLoginId, Integer busId, Feedback feedback) throws FeedbackException, UserException, BusException;
+//	public Feedback addFeedback(Integer userLoginId, Integer busId, Feedback feedback) throws FeedbackException, UserException, BusException;
 	
 	public Feedback updateFeedback(Feedback feedback) throws FeedbackException;
 	
