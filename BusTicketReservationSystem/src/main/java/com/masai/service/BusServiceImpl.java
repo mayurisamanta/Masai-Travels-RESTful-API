371c7e0189fa6783835ab1cf9615e9f1603a3100
Sarthak0008 <98695968+Sarthak0008@users.noreply.github.com>
2022-12-23 13:27:52 +0530

    some more comments added on Bus Service layer Implementation

--- BusTicketReservationSystem/src/main/java/com/masai/service/BusServiceImpl.java
+++ BusTicketReservationSystem/src/main/java/com/masai/service/BusServiceImpl.java
@@ -52,6 +52,7 @@ public class BusServiceImpl implements BusService{
 				if(route.getBuslist().contains(bus)) {
 					throw new BusException("Bus already exists");
 				}
+				
 //				if not present then add bus to buslist
 				route.getBuslist().add(bus);
 				bus.setRoute(route);
@@ -77,7 +78,7 @@ public class BusServiceImpl implements BusService{
 			Optional<Bus> opt=	busdao.findById(bus.getBusId());
 			if(opt.isPresent()) {
 				Bus curr = opt.get();
-				
+//			if seat is not available you cannot update bus details	
 				if (curr.getAvailabeSeats() != curr.getSeats()) 
 					throw new BusException("Cannot update Bus already scheduled");
 				
@@ -90,7 +91,7 @@ public class BusServiceImpl implements BusService{
 					if (route == null) 
 						throw new BusException("Invalid route details");
 				}
-			
+//		setting details	
 				if (bus.getArrivalTime() != null) curr.setArrivalTime(bus.getArrivalTime().toString());
 				if (bus.getAvailabeSeats() != null) curr.setAvailabeSeats(bus.getAvailabeSeats());
 				if (bus.getBusName() != null) curr.setBusName(bus.getBusName());
@@ -102,7 +103,7 @@ public class BusServiceImpl implements BusService{
 				if (bus.getSeats() != null) curr.setSeats(bus.getSeats());
 				
 				
-				
+//			save updated bus	
 				Bus updated = busdao.save(curr);
 				route.getBuslist().add(updated);
 				route.getBuslist().remove(bus);
@@ -116,6 +117,7 @@ public class BusServiceImpl implements BusService{
 		else throw new UserException("Unauthorized Access! Only Admin can make changes");
 
 	}
+//	overriding method implement delete bus method
 
 	@Override
 	public Bus deleteBus(Integer busId, String key)throws BusException, UserException {
@@ -125,6 +127,7 @@ public class BusServiceImpl implements BusService{
 			throw new UserException("Please provide a valid key to delete Bus");
 		}
 		if (loggedInUser.getType().equalsIgnoreCase("Admin")) {
+//			optional is used to handle npe
 			Optional<Bus> opt=	busdao.findById(busId);
 			if(opt.isPresent()) {
 				Bus exbus=opt.get();
@@ -142,7 +145,7 @@ public class BusServiceImpl implements BusService{
 		
 		
 	}
-
+//	overriding method implement  view bus by id  method
 	@Override
 	public Bus viewBus(Integer busId, String key) throws BusException, UserException{
 
@@ -159,13 +162,15 @@ public class BusServiceImpl implements BusService{
 		throw new BusException("bus doesn't exists with this "+busId+" id");
 	}
 
-
+//	overriding method implement view all bus method
 	@Override
 	public List<Bus> viewBusByType(String busType, String key) throws BusException, UserException {
 		CurrentUserSession loggedInUser=srepo.findByUuid(key);
 		if(loggedInUser==null) {
 			throw new UserException("Please provide a valid key to view buses");
 		}
+		
+//		finding list of bus
    List<Bus> bList = busdao.findByBusType(busType);
 		
 		if(bList.size()==0) {
@@ -181,6 +186,7 @@ public class BusServiceImpl implements BusService{
 		if(loggedInUser==null) {
 			throw new UserException("Please provide a valid key to view buses");
 		}
+//		anyone can view bus but it must be user or admin
 		List<Bus> buslist=	busdao.findAll();
 		if(buslist.size()==0) {
 			throw new BusException("bus list is empty");
