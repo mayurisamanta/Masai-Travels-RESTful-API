e8cba4691dc340893c1153a435aeea2b6a68ab28
mayurisamanta <samantamayuri@gmail.com>
2023-07-19 17:30:54 +0530

    Adding h2 configuration and change "user" to "user_account"

--- BusTicketReservationSystem/src/main/java/com/masai/model/User.java
+++ BusTicketReservationSystem/src/main/java/com/masai/model/User.java
@@ -1,29 +1,15 @@
 package com.masai.model;
 
-import java.util.Objects;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.UniqueElements;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @Entity
@@ -31,6 +17,7 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Table(name = "user_account")
 public class User {
 
 	@Id
